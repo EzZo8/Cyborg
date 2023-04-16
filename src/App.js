983d81950d94f7899b78142dfdb6654b1d3eb9ae
EzZo8 <ezzo4story@gmail.com>
2023-04-16 03:54:42 +0200

    adding gh-pages to  json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { HashRouter as Router , Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import {   Container } from './components/index';
 import {Header, Footer} from './sections/index';
 import {Home , Browse, Details} from './Pages/index'
 
 const App = () => {
+    
     return( 
         <>
         <Router>
